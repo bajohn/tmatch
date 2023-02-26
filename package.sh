facf689b5762884872fada3c92fe0cb6d279b3a8
Brendan John <bjohn454@gmail.com>
2023-02-25 16:56:59 -0800

    [BJ] server online

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@ cd ./server
 yarn build
 cd ..
 rm ./server/artifact/bundle.zip || true
-cp server/package.json norm-app/dist/package.json
-cd ./server/dist  
+cp server/package.json norm-app/transpiled/package.json
+cd ./server/transpiled  
 zip -r  ../artifact/bundle.zip *
